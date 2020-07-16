fffd67322fbd5ef07e915b4fab2296dd3875f00c
Forever-CodingNoob <johnnylin6256@gmail.com>
2020-07-16 15:54:35 +0800

    add favicon

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,url_for,redirect,render_template,flash,Request,request,session
 import sqlite3
-from scripts import Tag,Line,get_db_connection
+from scripts import Station
 import random
 import time
 import datetime
@@ -10,36 +10,8 @@ app.config['SECRET_KEY']="".join([chr(random.randint(21,126)) for i in range(10)
 print(app.secret_key)
 app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(minutes=2)
 
-class Station(dict):
-    def __init__(self,station,problem_number):
-        conn = get_db_connection()
-        contents = conn.execute(f'SELECT * FROM content_sorted WHERE station="{station}"').fetchall()
-        conn.close()
-        print(contents)
-        # problems=[{'type':content['type'],'exit':content['exit'],'content':content['content'],'answer':content['answer']} for content in contents]
-        content = contents[problem_number]
-        problem = {'number': problem_number, 'type': content['type'], 'exit': content['exit'],
-                   'content': content['content'], 'answer': content['answer']}
-        self.name= content['station']
-        self.lines= Line.getLine(content['lines_for_this_station'].split('/'))
-        self.grade= content['grade']
-        self.number_of_problems= len(contents)
-        super().__init__(problem)
-        self.tags = Tag.getTags(self)
-        print(self)
-'''
-def get_station(station,problem_number):
-    conn=get_db_connection()
-    contents=conn.execute(f'SELECT * FROM content_sorted WHERE station="{station}"').fetchall()
-    conn.close()
-    #problems=[{'type':content['type'],'exit':content['exit'],'content':content['content'],'answer':content['answer']} for content in contents]
-    content=contents[problem_number]
-    problem={'number':problem_number,'type': content['type'], 'exit': content['exit'], 'content': content['content'], 'answer': content['answer']}
-    station_infor = {'name':contents[0]['station'],'lines':contents[0]['lines_for_this_station'].split('/'),'grade':contents[0]['grade'],'number of problems':len(contents),'problem':problem}
-    
-    print(station_infor)
-    return station_infor
-'''
+
+
 @app.route('/<string:station>/<int:number>')
 def show_station(station,number):
     station=station.replace('_','/')#為避免'台北101/世貿'不能作為網址，故連結會導向'台北101_世貿'進入此函數，在此換成'台北101/世貿'!!!!!!!!!
@@ -49,10 +21,14 @@ def show_station(station,number):
 @app.route('/station/<string:station>')
 def just_show_station(station):
     return redirect(url_for('show_station',station=station,number=0))
-@app.route('/favicon.ico')
-def img():
-    print("brower is getting favortie icon but there's no favicon yet")
-    return "nope"
+
+
+
+
+# @app.route('/favicon.ico')
+# def img():
+#     print("brower is getting favortie icon....")
+#     return "nope"
 @app.route('/')
 def home():
     return render_template('index.html')
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3,os
 class Tag:
     @classmethod
     def getTags(cls, station):
@@ -39,7 +39,48 @@ class Line:
     @classmethod
     def getLine(cls,lineList):
         return [{'name':line,'imgSRC':f'img/{cls.toEN(line)}.png'} for line in lineList]
+
+
+class Station(dict):
+    def __init__(self,station,problem_number):
+        conn = get_db_connection()
+        contents = conn.execute(f'SELECT * FROM content_sorted WHERE station="{station}"').fetchall()
+        conn.close()
+        print(contents)
+        # problems=[{'type':content['type'],'exit':content['exit'],'content':content['content'],'answer':content['answer']} for content in contents]
+        content = contents[problem_number]
+        problem = {'number': problem_number, 'type': content['type'], 'exit': content['exit'],
+                   'content': content['content'], 'answer': content['answer']}
+        self.name= content['station']
+        self.lines= Line.getLine(content['lines_for_this_station'].split('/'))
+        self.grade= content['grade']
+        self.number_of_problems= len(contents)
+        super().__init__(problem)
+        self.tags = Tag.getTags(self)
+        print(self)
+
+
+'''
+def get_station(station,problem_number):
+    conn=get_db_connection()
+    contents=conn.execute(f'SELECT * FROM content_sorted WHERE station="{station}"').fetchall()
+    conn.close()
+    #problems=[{'type':content['type'],'exit':content['exit'],'content':content['content'],'answer':content['answer']} for content in contents]
+    content=contents[problem_number]
+    problem={'number':problem_number,'type': content['type'], 'exit': content['exit'], 'content': content['content'], 'answer': content['answer']}
+    station_infor = {'name':contents[0]['station'],'lines':contents[0]['lines_for_this_station'].split('/'),'grade':contents[0]['grade'],'number of problems':len(contents),'problem':problem}
+
+    print(station_infor)
+    return station_infor
+'''
 def get_db_connection():
-    conn=sqlite3.connect('stations.sqlite')
+    # print(__file__) #此.py檔絕對路徑
+    # print(os.path.sep) #路徑分隔符"\"
+    # print(__file__.split(os.path.sep)) #split the path=>list
+    # print(__file__.split(os.path.sep)[:-2]) #去掉最後2個子目錄
+    # print(os.sep.join(__file__.split(os.sep)[:-2])) #list組回path
+    path=os.path.join(os.sep.join(__file__.split(os.sep)[:-2]),r'data\stations.sqlite')#加上.splite3的路徑
+    print('path=',path)
+    conn=sqlite3.connect(path)
     conn.row_factory=sqlite3.Row
     return conn
